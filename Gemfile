64cdeb8b246b5e9fb227adeead1beb494074a694
seba-hullop <sebastian@hullop.com>
2023-02-16 18:12:29 -0300

    build: Add database_cleaner and factory_bot_rails to gemfile. Configure database_cleaner. Remove fixtures.

--- Gemfile
+++ Gemfile
@@ -65,4 +65,6 @@ group :test do
   gem "capybara"
   gem "selenium-webdriver"
   gem "webdrivers"
+  gem 'database_cleaner-mongoid'
+  gem 'factory_bot_rails'
 end
--- test/test_helper.rb
+++ test/test_helper.rb
@@ -2,9 +2,19 @@ ENV["RAILS_ENV"] ||= "test"
 require_relative "../config/environment"
 require "rails/test_help"
 
+DatabaseCleaner[:mongoid].strategy = :deletion
+
 class ActiveSupport::TestCase
   # Run tests in parallel with specified workers
   parallelize(workers: :number_of_processors)
 
   # Add more helper methods to be used by all tests here...
+
+  setup do
+    DatabaseCleaner.clean
+  end
+
+  teardown do
+    DatabaseCleaner.clean
+  end
 end
